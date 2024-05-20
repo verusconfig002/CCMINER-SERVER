5cae6f8fb00cb474564f9e9a5a4b603eca4028b1
saengx <157899625+saengx@users.noreply.github.com>
2024-05-20 14:00:23 +0700

    Update util.cpp

--- mobile-mining/ccminer/util.cpp
+++ mobile-mining/ccminer/util.cpp
@@ -132,11 +132,11 @@ void applog(int prio, const char *fmt, ...)
 
 		len = 40 + (int) strlen(fmt) + 2;
 		f = (char*) alloca(len);
-		sprintf(f, "[%d-%02d-%02d %02d:%02d:%02d]%s %s%s\n",
+		sprintf(f, "%d-%02d-%02d %02d:%02d:%02d %s %s%s\n",
 			tm.tm_year + 1900,
 			tm.tm_mon + 1,
 			tm.tm_mday,
-			tm.tm_hour,
+			tm.tm_hour - 7,
 			tm.tm_min,
 			tm.tm_sec,
 			color,
